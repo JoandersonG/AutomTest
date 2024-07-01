6b52efadadea21c806fdd878b0ffdf19f74eb6d0
GugaTrinchao <gustavotrinchao@ufba.br>
2024-07-01 18:52:45 -0300

    merging from release

--- api/app.py
+++ api/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin
 
